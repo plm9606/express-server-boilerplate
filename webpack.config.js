87c77e120d2a2cbca2acb4cc925e055207db58b9
plm9606 <plm9606@naver.com>
2019-10-01 11:12:05 +0900

    chore: webpack scss 컴파일을 위한 모듈 추가

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,15 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader', // compiles Sass to CSS, using Node Sass by default
+        ],
+        exclude: /node_modules/,
+      },
     ],
   },
   devtool: 'source-map',
